65c66649cf7ffbc6e8ea43bb39fb8d28a565031c
antonio481 <adanilovx@gmail.com>
2020-06-02 11:45:00 +0400

    Изменения тест мейн пейдж

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,4 @@
-from .main_page import MainPage
+from .pages.main_page import MainPage
 link = "http://selenium1py.pythonanywhere.com/"
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
